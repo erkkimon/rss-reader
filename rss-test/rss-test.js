43ca6e255b15bb0e687f3ef2890471a0f67a22b9
Erkkimon <tom.himanen@gmail.com>
2015-08-12 12:54:24 +0300

    RSS feed fetching works now

--- rss-test/rss-test.js
+++ rss-test/rss-test.js
@@ -11,10 +11,17 @@ if (Meteor.isClient) {
 }
 
 if (Meteor.isServer) {
-  websiteData = Scrape.website("http://www.tiede.fi")
-  News.insert({name: websiteData.lang});
-  News.insert({name: "Erkki"});
-  News.insert({name: "Mauno"});
+  //News.insert({name: websiteData.lang});
+var rssSources = ['http://news.yandex.ru/auto.rss', "http://yle.fi/uutiset/rss/paauutiset.rss"];
+parsedRss = [];
+var rssContent = "";
+for (var i = 0; i < rssSources.length; i++) {
+  var result = Meteor.http.call("GET", rssSources[i]);
+  if(result.statusCode == '200' && result.content){
+     rssContent += result.content;
+  }
+}
+console.log(rssContent);
   Meteor.startup(function () {
     // code to run on server at startup
   });
