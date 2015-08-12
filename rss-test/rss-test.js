6c55ee3cd81f52a4e9a309c35b75916874505f62
Erkkimon <tom.himanen@gmail.com>
2015-08-12 20:24:02 +0300

    XML parsing tested and working

--- rss-test/rss-test.js
+++ rss-test/rss-test.js
@@ -11,18 +11,24 @@ if (Meteor.isClient) {
 }
 
 if (Meteor.isServer) {
-  //News.insert({name: websiteData.lang});
-var rssSources = ['http://news.yandex.ru/auto.rss', "http://yle.fi/uutiset/rss/paauutiset.rss"];
-parsedRss = [];
-var rssContent = "";
-for (var i = 0; i < rssSources.length; i++) {
-  var result = Meteor.http.call("GET", rssSources[i]);
-  if(result.statusCode == '200' && result.content){
-     rssContent += result.content;
-  }
-}
-console.log(rssContent);
-  Meteor.startup(function () {
-    // code to run on server at startup
-  });
+  //var rssSources = ['http://news.yandex.ru/auto.rss', "http://yle.fi/uutiset/rss/paauutiset.rss"];
+  //for (var i = 0; i < rssSources.length; i++) {
+  //  var resultJSON;
+  //  var resultXML = Meteor.http.call("GET", rssSources[i]);
+  //  if(resultXML.statusCode == '200' && resultXML.content) {
+  //    //console.log(resultXML.content);
+  //    resultJSON = xml2js.parseString(resultXML, function(err, result) {
+  //      console.log(result['data']);
+  //    })
+  //  }
+  //}
+var xml = "<config><test>Hello</test><data>SomeData</data></config>";
+
+var extractedData = "";
+xml2js.parseString(xml, function(err,result){
+  //Extract the value from the data element
+  extractedData = result['config']['data'];
+  console.log(extractedData);
+});
+console.log("Note that you can't use value here if parseString is async; extractedData=", extractedData);
 }
