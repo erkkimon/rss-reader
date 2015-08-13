b12bb7d7e0b5875464ce2892807e5bcbbd9f6cf0
Erkkimon <tom.himanen@gmail.com>
2015-08-13 13:04:40 +0300

    url2json implemented and tested

--- rss-test/rss-test.js
+++ rss-test/rss-test.js
@@ -11,6 +11,7 @@ if (Meteor.isClient) {
 }
 
 if (Meteor.isServer) {
+
   //var rssSources = ['http://news.yandex.ru/auto.rss', "http://yle.fi/uutiset/rss/paauutiset.rss"];
   //for (var i = 0; i < rssSources.length; i++) {
   //  var resultJSON;
@@ -18,26 +19,36 @@ if (Meteor.isServer) {
   //  if(resultXML.statusCode == '200' && resultXML.content) {
   //    //console.log(resultXML.content);
   //    resultJSON = xml2js.parseString(resultXML, function(err, result) {
-  //      console.log(result['data']);
+  //      console.log(result);
   //    })
   //  }
   //}
-  var xml = "<config><test>Hello</test><data>SomeData</data></config>";
+
   
-  function eatXMLvomitJSON(inputXML) {
+
+  //var xml = "<config><test>Hello</test><data>SomeData</data></config>";
+  
+
+  function url2json(inputURL) {
+    var inputXML = Meteor.http.call("GET", inputURL)["content"];
     var outputJSON;
     xml2js.parseString(inputXML, function(err, res) {
-      outputJSON = res;
+      outputJSON = res.rss.channel;
     });
     return outputJSON; 
   }
 
-  console.log(eatXMLvomitJSON(xml));
+  console.log(url2json("http://yle.fi/uutiset/rss/paauutiset.rss"));
 
-  var extractedData = "";
-  xml2js.parseString(xml, function(err,result){
-    //Extract the value from the data element
-    extractedData = result['config']['data'];
-    console.log(extractedData);
-  });
+  //console.log(rssFeedXML);
+  //console.log(rssFeedXML.content);
+  //console.log(eatXMLvomitJSON(rssFeedXML[0][1]));
+
+
+  //var extractedData = "";
+  //xml2js.parseString(xml, function(err,result){
+  //  //Extract the value from the data element
+  //  extractedData = result['config']['data'];
+  //  console.log(extractedData);
+  //});
 }
