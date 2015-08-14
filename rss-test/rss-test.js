8e0a6f9b75a9db79a4aa8f5f718019b49a27c8b1
Erkkimon <tom.himanen@gmail.com>
2015-08-14 03:52:23 +0300

    url2json() doesn't explode anymore when facing 404

--- rss-test/rss-test.js
+++ rss-test/rss-test.js
@@ -13,21 +13,27 @@ if (Meteor.isClient) {
 if (Meteor.isServer) {
 
   function url2json(inputURL) {
-    var inputXML = Meteor.http.call("GET", inputURL)["content"];
-    var outputJSON;
-    xml2js.parseString(inputXML, function(err, res) {
-      outputJSON = res.rss.channel[0];
-    });
-    return outputJSON; 
+    outputJSON = null;
+    try {
+      var inputXML = HTTP.get(inputURL);
+      xml2js.parseString(inputXML["content"], function(err, res) {
+        outputJSON = res.rss.channel[0];
+      });
+    } catch(e) {
+      console.log("url2json() failed to fetch and jsonify url " + inputURL);
+    }
+    return outputJSON;
   }
 
+  console.log(url2json("http://yle.fi/uutiset/rss/paauutiset.rss"));
+
   // Do necessary stuff to a single feed
-  feedJSON = url2json("http://yle.fi/uutiset/rss/paauutiset.rss");
-  for(var i in feedJSON.item) {
-    var feedItem = feedJSON.item[i];
-    News.insert({
-      "title": feedItem.title
-    });
-  }
+  //feedJSON = url2json("http://yle.fi/uutiset/rss/paauutiset.rss");
+  //for(var i in feedJSON.item) {
+  //  var feedItem = feedJSON.item[i];
+  //  News.insert({
+  //    "title": feedItem.title
+  //  });
+  //}
 
 }
